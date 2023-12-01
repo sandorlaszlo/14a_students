cb05fd955fc6bba38361902756b6f0883e749c26
Sandor Laszlo <slo@jedlik.eu>
2023-12-01 13:24:12 +0100

    Student CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource("Student",StudentController::class);
+Route::apiResource("Teacher", TeacherController::class);
